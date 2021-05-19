8cb20e5c1887678cc48c5d1abcc9c4bccee85094
h-cue <hmabrantes@hotmail.com>
2021-05-19 15:05:59 +0200

    react base

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Top from './components/Top.js';
+
+function App() {
+  return (
+    <div className="App">
+      <Top 
+        className="Top"
+      />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Top.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Top = props => {
+    return <p>Hej</p>
+}
+
+export default Top;
+
