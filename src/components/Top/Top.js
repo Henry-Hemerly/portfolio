8cd53b976c605f05f076a0ea151f9488cc07091e
h-cue <hmabrantes@hotmail.com>
2021-05-20 10:12:36 +0200

    further head

--- src/components/Top/Top.js
+++ src/components/Top/Top.js
@@ -4,9 +4,11 @@ import './Top.css'
 const Top = props => {
     return (
         <div className='wrapper'>
-            <div className='content'>
-                <h1 className='hero-title'>Let's build something amazing</h1>
-                <p className='hero-p'>My name's Henry, welcome to my portfolio</p>
+            <div className='left-wrapper'>
+                <div className='content'>
+                    <h1 className='hero-title'>Let's build something amazing</h1>
+                    <p className='hero-p'>My name's Henry, I'm a developer and a designer. Welcome!</p>
+                </div>
             </div>
         </div>
     ) 
