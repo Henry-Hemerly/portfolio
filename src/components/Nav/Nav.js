6c8ff759c5bc701db0848ff955dd2b652af54527
h-cue <hmabrantes@hotmail.com>
2021-05-20 17:16:34 +0200

    navigation adds

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,10 +1,19 @@
 import React from 'react';
-import './Nav.css';
+import styles from './Nav.module.css';
 
 const Nav = props => {
     return (
-        <div className='wrapperl'>
-
+        <div className={styles.wrapper}>
+            <div className={styles.line}></div>
+            <div className={styles.dots}>
+              <ul className={styles.list}>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </div>
+            <div className={styles.line}></div>
         </div>
     )
 }
--- src/components/Top/Top.js
+++ src/components/Top/Top.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import './Top.css';
+import styles from './Top.module.css';
 import Nav from '../Nav/Nav.js';
 
 const Top = props => {
     return (
-        <div className='wrapper'>
-            <div className='left-wrapper'>
-                <div className='content'>
-                    <h1 className='hero-title'>Let's build something amazing</h1>
-                    <p className='hero-p'>Tjenna! My name's Henry, I'm a developer and a graphic designer. Welcome to my portfolio!</p>
-                    <div className='hero-button'>
-                        <p className='hero-button-text'>Let's talk</p>
+        <div className={styles.wrapper}>
+            <div className={styles.left_wrapper}>
+                <div className={styles.content}>
+                    <h1 className={styles.hero_title}>Let's build something amazing</h1>
+                    <p className={styles.hero_p}>Tjenna! My name's Henry, I'm a developer and a graphic designer. Welcome to my portfolio!</p>
+                    <div className={styles.hero_button}>
+                        <p className={styles.hero_button_text}>Let's talk</p>
                     </div>
                 </div>
             </div>
